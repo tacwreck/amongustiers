e633c674ac9770796f144a6dc0d6ecd6e2d102b2
tac <wrecktactical@gmail.com>
2025-11-08 15:25:28 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 // Configurable links
 const config = {
-  title: "Minecraft Server | Maintenance",
-  discord: "https://discord.gg/yourserver",
-  vote: "https://minecraft-server-vote-link.com",
-  store: "https://yourstore.com"
+  title: "Amongus Ranked | Maintenance",
+  discord: "discord.gg/thzYmcV3dnr",
 };
+
